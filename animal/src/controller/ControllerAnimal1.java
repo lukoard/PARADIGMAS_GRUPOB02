6e0638abfb5deaecb5d4f038e843e2641139db68
grs89 <gr.savedra11@gmail.com>
2022-06-27 22:08:55 -0500

    intento 3

--- animal/src/controller/ControllerAnimal1.java
+++ animal/src/controller/ControllerAnimal1.java
@@ -2,27 +2,22 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ControllerAnimal1 {
 	
-	private ControllerInicio ConInicio;
-	private Stage stage;
-	
     @FXML
     private AnchorPane NameAnimal;
 
     @FXML
     private TextField nameAnimal;
-    	
-    @FXML
-    void close(ActionEvent event) {
-    	ConInicio.show();
-    	stage.show();
-    }
 
+    @FXML
+    private Button close;
+    
     @FXML
     void b50d0d(ActionEvent event) {
 
@@ -30,11 +25,9 @@ public class ControllerAnimal1 {
     
     @FXML
     void onEnter(ActionEvent event) {
-    	System.out.println("test") ;
+    	
     }
-    
- 
-    
+         
 	public void init(Object object, Stage stage2, ControllerInicio controllerInicio) {
 				
 	}
--- animal/src/controller/ControllerAnimal2.java
+++ animal/src/controller/ControllerAnimal2.java
@@ -2,29 +2,29 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-//import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage; 
 
 public class ControllerAnimal2 {
 
-	private ControllerInicio ConInicio;
-	private Stage stage;
-//	private Label lblName;
-	
     @FXML
-    void close(ActionEvent event) {
-    	ConInicio.show();
-    	stage.close();
-
-    }
+    private TextField nameAnimal;
+    
+    @FXML
+    private Button close;
 
     @FXML
     void b50d0d(ActionEvent event) {
 
     }
+    
+    @FXML
+    void onEnter(ActionEvent event) {
+    	
+    }
 
-    public void init(Object object, Stage stage2, ControllerInicio controllerInicio) {
-		// TODO Auto-generated method stub
+    public void init(Object object, Stage stage2, ControllerInicio controllerInicio) {	
 		
 	}
 
